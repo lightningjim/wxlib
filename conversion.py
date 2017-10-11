a2aed139f690da3372b2ae62a789fb764393a9e6
Kyle Creasey <kcreasey@so2.infinitysrv.com>
2017-10-10 20:24:26 -0500

    Second stage, added mixing ratio equation

--- conversion.py
+++ conversion.py
@@ -18,21 +18,89 @@ s_mmmg = 2
 
 def temp_conv(temp, from_unit, to_unit):
 	if from_unit == s_fahrenheit:
-		temp_conv = {
-			s_fahrenheit : temp,
-			s_celsius : 5 / 9 * (temp - 32),
-			s_kelvin : 5 / 9 * (temp + 459.67)
-		}
-	if from_unit == s_celsius:
-		temp_conv = {
-			s_fahrenheit : 9 / 5 * temp + 32,
-			s_celsius : temp,
-			s_kelvin : temp + 273.15
-		}
-	if from_unit == s_kelvin:
-		temp_conv = {
-			s_fahrenheit : temp * 9 / 5 - 459.67,
-			s_celsius : temp + 273.15,
-			s_kelvin : temp
-		}
-	return temp_conv(to_unit, None)
+		if to_unit == s_fahrenheit:
+			return temp
+		elif to_unit == s_celsius:
+			return 5 / 9 * (temp - 32)
+		elif to_unit == s_kelvin:
+			return 5 / 9 * (temp + 459.67)
+		else:
+			return None
+
+	elif from_unit == s_celsius:
+		if to_unit == s_celsius:
+			return temp
+		elif to_unit == s_kelvin:
+			return temp + 273.15
+		elif to_unit == s_fahrenheit:
+			return 9 / 5 * temp + 32
+		else:
+			return None
+
+	elif from_unit == s_kelvin:
+		if to_unit == s_kelvin:
+			return temp
+		elif to_unit == s_fahrenheit:
+			return temp * 9 / 5 - 459.67
+		elif to_unit == s_celsius:
+			return temp + 273.15
+		else:
+			return None
+	else:
+		return None
+
+def wind_conv(speed, from_unit, to_unit):
+	if from_unit == s_mph:
+		if to_unit == s_mph:
+			return speed
+		elif to_unit == s_knots:
+			return 1069.344 / 1852 * speed
+		elif to_unit == s_kph:
+			return 1.609344 * speed
+		elif to_unit == s_ms:
+			return 0.44704 * speed
+		else:
+			return None
+	elif from_unit == s_knots:
+		if to_unit == s_knots:
+			return speed
+		elif to_unit == s_kph:
+			return 1.852 * speed
+		elif to_unit == s_ms:
+			return 1852.0 / 3600 * speed
+		elif to_unit == s_mph:
+			return 1.852 / 1.609344 * speed
+		else:
+			return None
+	elif from_unit == s_kph:
+		if to_unit == s_kph:
+			return speed
+		elif to_unit == s_ms:
+			return 1 / 3.6 * speed
+		elif to_unit == s_mph:
+			return 1 / 1.609344 * speed
+		elif to_unit == s_knots:
+			return 1 / 1.852 * speed
+		else:
+			return None
+	elif from_unit == s_ms:
+		if to_unit == s_ms:
+			return speed
+		elif to_unit == s_mph:
+			return 1 / 0.44704 * speed
+		elif to_unit == s_knots:
+			return 3.6 / 1.852 * speed
+		elif to_unit == s_kph:
+			return 3.6 * speed
+		else:
+			return None
+	else:
+		return None
+
+def rh_from_dp(temp, dp):
+	return 100 * (math.exp(17.625 * dp) / (243.04 + dp)) / math.exp((17.625 * temp) / (243.04 + temp))
+
+def gkg_from_dp(dp, pres):
+	v = 1 - ((dp.toK()) / 647.096)
+	pw = 220640 * log((647.906 / dp.toK()) * -7.85951783 * v * + 1.84408259 * power(v,1.5) - 11.7866497 * power(v,3) + 22.6807411 * power(v,3.5) - 15.9618719 * power(v,4) + 1.80122502 * power(v,7.5))
+	return 621.9907 * pw / (pres - pw)
--- get_okmeso.py
+++ get_okmeso.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
 import urllib.request as urllib2
-#import csv
+import csv
+import codecs
+import argparse
+from mesonet import mesonet
 
-#url = "http://mesonet.org/data/basic/mesonet/current/current.csv.txt"
-url = "http://www.mesonet.org/data/basic/mesonet/latest/latest.mdf"
-wxdata = urllib2.urlopen(url)
+parser = argparse.ArgumentParser(description='Decode Mesonet data')
+parser.add_argument('--station',dest='station', default='OKCE', help='Station ID you are looking up')
+
+args = parser.parse_args()
+
+url = "http://www.mesonet.org/data/basic/mesonet/current/current.csv.v4.txt"
+response = urllib2.urlopen(url)
+wxdata = csv.reader(codecs.iterdecode(response,'utf-8'),delimiter=',')
 for row in wxdata:
-	print(row.split())
-#	if row[0] == "OKCN":
-#		print("Oklahoma City North at " + row[8] + ":" + row[9])
-#		print("Temp: " + row[10] + "F")
-#		print("Humidity: \n\tDew Point: " + row[11] + "F\n\tRH: " + row[12] + "%")	
-#		print("Wind: \n\t" + row[15] + " at " + row[16] + " MPH\n\tgusting to " + row[17] + " MPH")
+	# Headers
+	if row[0] == "STID":
+		print(row)
+	if row[0] == args.station:
+		print(row)
+		test = mesonet(row)
+		print(test)

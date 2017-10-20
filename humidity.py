61ea69c8cb9878ff8003c489f99422897973b081
Kyle Creasey <kcreasey@so2.infinitysrv.com>
2017-10-20 17:01:30 -0500

    Multiple instantiation for Humidity (Abs/RH default, other methods added)

--- humidity.py
+++ humidity.py
@@ -1,20 +1,31 @@
 import conversion
+import temp
 class humidity(object):
 	s_rh_only = 0
 	s_td_only = 1
 	s_gkg = 2
 
-	def __init__(self, humid, air, htype):
-		if htype == s_rh_only:
-			self.relative = humid
-			#self.absolute = dp_from_rh(self.relative, air.toC())
-		elif htype == s_td_only:
-			self.absolute == humid
-			self.relative = rh_from_dp(self.absolute.toC(), air.toC())
-		elif htype == s_gkg:
-			#self.absolute == gkg_to_dp(humid)
-			self.none = None
-
+	def __init__(self, *args):
+		# Absolute and Relative Direct
+		if len(args) == 2:
+			self.absolute = args[0]
+			self.relative = args[1]
+		# Only has RH or Absolute
+		elif len(args) == 3:
+			# If only has Relative
+			if args[2] == s_rh_only:
+				self.relative = args[0]
+				self.absolute = dp_from_rh(args[1], args[2].toC())
+			# If only has Absolute as Dewpoint
+			elif args[2] == s_td_only:
+				self.absolute = args[1]
+				self.relative = rh_from_dp(args[1].toC(), args[2].toC())
+			elif args[2] == s_gkg:
+				self.absolute = gkg_to_dp(args[1])
+				self.relative = rh_from_dp(self.absolute.toC(),args[2].toC())
+		else:
+				self.relative = -1
+				self.absolute = temp(0, conversion.s_kelvin)
 
 	def __init__(self, absolute, relative):
 		self.absolute = absolute
